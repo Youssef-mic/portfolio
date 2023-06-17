d8bcd9c880f4dedb90360a21e79ceacd32a9a723
Youssef Michael <ym262010@gmail.com>
2023-06-17 11:24:31 +0300

    Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,57 @@
 import Image from 'next/image'
-import styles from './page.module.css'
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section className='section-0' id="top">
+        <Image src={"/Logo.png"} width={500} height={500} className='logo' alt='logo'></Image>
+        <div className='title'>
+          <h1>Youssef Michael</h1>
+          <h4>I'm a programmer who is <section className="animation">
+            <div className="first">
+              <div>young</div>
+            </div>
+            <div className="second">
+              <div>practical</div>
+            </div>
+            <div className="third">
+              <div>ambitios</div>
+            </div>
+          </section></h4>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      <section className="section-1">
+        <h2 className='about'>About Me</h2>
+        <p>I'm a 13 y/o developer in Syria, I started about 4 years ago, my goal is to create open-source, practical solutions for every day problems</p>
+      </section>
+      <section className="section-2">
+        <h2 className='technologies'>Technologies</h2>
+        <div className='container-0'>
+          <div className='lang-container'>
+            <Image src={"/icons8-c++(1).svg"} width={100} height={100} alt='C++'></Image>
+            <h3>C++</h3>
+          </div>
+          <div className='lang-container'>
+            <Image src={"/icons8-javascript.svg"} width={100} height={100} alt='JavaScript'></Image>
+            <h3>JavaScript</h3>
+          </div>
+          <div className='lang-container'>
+            <Image src={"/icons8-next.js.svg"} width={100} height={100} alt='NextJS'></Image>
+            <h3>NextJS</h3>
+          </div>
+          <div className='lang-container'>
+            <Image src={"/icons8-react-native(1).svg"} width={100} height={100} alt='ReactJS'></Image>
+            <h3>ReactJS</h3>
+          </div>
+        </div>
+      </section>
+      <section className="section-3">
+        <h2 className='contact'>Contact Me</h2>
+        <div>
+          <p>You can contact me through my <a href="mailto:ym262010@gmail.com">email</a></p>
+          <p>Also, you can see my projects on <a href="https://github.com/youssef-mic">github</a></p>  
+        </div>
+      </section>
     </main>
   )
 }
